4bf522961e3297bdbdba1cfca05342b0678266e3
asrafkhan-54 <md178983@gmail.com>
2025-03-19 13:50:56 +0600

    1st commit

---
+++ example.R
@@ -0,0 +1,13 @@
+"Puppy Cup"
+Copyright (C) 2024 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
